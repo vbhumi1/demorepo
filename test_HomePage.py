195862fddce08145eadf0e7ede5fa634107592fc
vijay <vbhumi.py@gmail.com>
2021-01-10 16:12:07 +0530

    First Commit

---
+++ test_HomePage.py
@@ -0,0 +1,37 @@
+
+from selenium.webdriver.support.select import Select
+from selenium import webdriver
+import pytest
+
+from TestData.HomePageData import HomePageData
+from pageObjects.HomePage import HomePage
+from utilities.BaseClass import BaseClass
+
+
+class TestHomePage(BaseClass):
+
+    def test_formSubmission(self,getData):
+        log = self.getLogger()
+		
+		print("adding in second file for push and pull")
+		
+		print("branch in all module")
+		
+        homepage= HomePage(self.driver)
+        log.info("first name is "+getData["firstname"])
+        homepage.getName().send_keys(getData["firstname"])
+        homepage.getEmail().send_keys(getData["lastname"])
+        homepage.getCheckBox().click()
+        self.selectOptionByText(homepage.getGender(), getData["gender"])
+
+        homepage.submitForm().click()
+
+        alertText = homepage.getSuccessMessage().text
+
+        assert ("Success" in alertText)
+        self.driver.refresh()
+
+    @pytest.fixture(params=HomePageData.getTestData("Testcase2"))
+    def getData(self, request):
+        return request.param
+
